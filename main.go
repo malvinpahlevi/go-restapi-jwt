1685c11f414f563b1bb83beb92d5109546ae58d7
malvinpahlevi <malvinfrog@gmail.com>
2022-11-05 21:21:13 +0700

    edit comment

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	// public like base url "/api/"
+	// public like base url "/api/v1"
 	public := router.Group("/api/v1")
 
 	public.POST("/register", func(c *gin.Context) {
