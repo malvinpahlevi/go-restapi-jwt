9f70e7b3b914abd0afbe04534a97e8e0373e8ecd
@malvinpahlevi <malvinfrog@gmail.com>
2022-11-05 20:50:40 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+
+	// public like base url "/api/"
+	public := router.Group("/api/v1")
+
+	public.POST("/register", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"data": "this is a register endpoint",
+		})
+	})
+
+	router.Run(":8080")
+
+}
